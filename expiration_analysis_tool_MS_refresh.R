d98e774d3c9c68be2ee9fc4bf0351dd6beeb6f4c
slee <sanlee8495@gmail.com>
2022-08-19 13:40:39 -0700

    8/19/22

--- expiration_analysis_tool_MS_refresh.R
+++ expiration_analysis_tool_MS_refresh.R
@@ -10,7 +10,7 @@ library(lubridate)
 
 
 ####################################### File read ##################################
-# Inventory Lot Details ----
+# (Path revision Needed) Inventory Lot Details ----
 inv_lot_details <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/Shippable Tool Creation/Automation/expiration_for_R.xlsx",
                               sheet = "Inventory Lot Detail")
 
@@ -27,11 +27,6 @@ inv_lot_details %>%
                 calculated_shippable_date = as.Date(calculated_shippable_date, origin = "1899-12-30"),
                 expiration_date = as.Date(expiration_date, origin = "1899-12-30"),
                 last_purchase_price = round(last_purchase_price, 2)) -> inv_lot_details
-  
-
-
-
-##################################### ETL ####################################
 
 # supply_pivot 
 inv_lot_details %>% 
@@ -41,6 +36,81 @@ inv_lot_details %>%
 
 supply_pivot -> analysis_ref.2
 as.data.frame(analysis_ref.2) -> analysis_ref.2
+
+
+  
+# (Path revision Needed) Custord ----
+custord <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/Shippable Tool Creation/Automation/expiration_for_R.xlsx",
+                              sheet = "CustOrd")
+
+
+custord[-1, ] -> custord
+colnames(custord) <- custord[1, ]
+custord[-1, ] -> custord
+
+custord %>% 
+  janitor::clean_names() %>% 
+  readr::type_convert() %>% 
+  dplyr::rename(sku = product_label_sku) %>% 
+  dplyr::mutate(sales_order_requested_ship_date = as.Date(sales_order_requested_ship_date, origin = "1899-12-30"),
+                ref = paste0(location, "_", sku)) %>% 
+  dplyr::relocate(ref) -> custord
+
+# custord_pivot
+custord %>% 
+  dplyr::group_by(ref) %>% 
+  dplyr::summarise(sum_of_open_order_cases = sum(open_order_cases)) %>% 
+  as.data.frame() %>% 
+  dplyr::mutate(custord_daily_avg = sum_of_open_order_cases / 15,
+                custord_daily_avg = round(custord_daily_avg, 0)) -> custord_pivot
+
+
+
+# (Path revision Needed) planner address book (If updated, correct this link) ----
+planner_address <- read_excel("S:/Supply Chain Projects/Linda Liang/reference files/Address Book - 08.04.22.xlsx")
+planner_address %>% 
+  janitor::clean_names() %>% 
+  readr::type_convert() %>% 
+  dplyr::select(1:2) %>% 
+  dplyr::rename(planner_number = address_number,
+                planner_name = alpha_name) -> planner_address
+
+
+# (Path revision Needed) exception report ----
+exception_report <- read_excel("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 23/Shippable Tool Creation/Automation/exception report 08.17.22 (1).xlsx")
+
+exception_report[-1:-2, ] -> exception_report
+colnames(exception_report) <- exception_report[1, ]
+exception_report[-1, ] -> exception_report
+
+exception_report %>% 
+  janitor::clean_names() %>% 
+  readr::type_convert() %>% 
+  dplyr::rename(location = b_p,
+                sku = item_number,
+                planner_number = planner) %>% 
+  dplyr::select(location, sku, planner_number) %>% 
+  dplyr::mutate(ref = paste0(location, "_", sku)) %>% 
+  dplyr::mutate(planner_number = replace(planner_number, is.na(planner_number), 0)) -> exception_planner
+
+
+exception_planner %>% filter(ref == "203_20624WCP")
+exception_planner %>% filter(ref == "208_17680CGB")
+
+##################################### ETL ####################################
+
+# Planner #
+merge(analysis_ref.2, exception_planner[, c("ref", "planner_number")], by = "ref", all.x = TRUE) %>% 
+  dplyr::relocate(planner_number, .after = description) %>% 
+  dplyr::mutate(planner_number = replace(planner_number, is.na(planner_number), "DNRR")) -> a
+
+a %>% filter(ref == "203_20624WCP")
+
+
+  # Planner Name
+merge(analysis_ref.2, planner_address[, c("planner_number", "planner_name")], by = "planner_number", all.x = TRUE) %>% 
+  dplyr::relocate(c(planner_number, planner_name), .after = description) -> analysis_ref.2
+
 
 # Days left on SSL
 analysis_ref.2 %>% 
@@ -92,9 +162,22 @@ dummy %>%
                 dummy_days_left_on_ssl = days_left_on_ssl) %>% 
   dplyr::bind_cols(analysis_ref.2) %>% 
   dplyr::mutate(diff_factor = ifelse(dummy_ref == ref & dummy_days_left_on_ssl > 0, days_left_on_ssl - dummy_days_left_on_ssl, 0)) %>% 
-  dplyr::relocate(diff_factor, .after = inventory_in_cost) %>% 
-  dplyr::select(-dummy_ref, -dummy_days_left_on_ssl) -> analysis_ref.2
+  dplyr::relocate(diff_factor, .after = inventory_in_cost) -> analysis_ref.2
 
+
+
+# Total CustOrd (within 15 days)
+merge(analysis_ref.2, custord_pivot[, c("ref", "sum_of_open_order_cases")], by = "ref", all.x = TRUE) %>% 
+  dplyr::rename(total_custord_within_15_days = sum_of_open_order_cases) %>% 
+  dplyr::mutate(total_custord_within_15_days = replace(total_custord_within_15_days, is.na(total_custord_within_15_days), 0)) -> analysis_ref.2
+
+
+# Inv after Custord
+analysis_ref.2 %>% 
+  dplyr::mutate(inv_after_custord = ifelse(days_left_on_ssl <= 0, sum_of_inventory_qty ,
+                                           ifelse(ref = dummy_ref, 
+                                                  ifelse(dummy_days_left_on_ssl > 0 & S3 > 0, sum_of_inventory_qty, 
+                                                         SUMIFS($O$4:O4, $A$4:A4, $A4, $H$4:H4, ">0") -R3),O4-R4)))
 
 
 
